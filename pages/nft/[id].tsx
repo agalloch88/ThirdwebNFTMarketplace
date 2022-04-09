dd14e434d29e62bf0bcaa40f4e0c8962556bcfe3
Ryan Kirsch <agalloch88@gmail.com>
2022-04-09 17:02:44 -0400

    finish implementing login

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -6,6 +6,9 @@ function NFTDropPage() {
     const connectWithMetamask = useMetamask();
     const address = useAddress();
     const disconnect = useDisconnect();
+
+    console.log(address);
+    
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left */}
@@ -38,11 +41,14 @@ function NFTDropPage() {
             </span>{' '}
             NFT Marketplace
           </h1>
-          <button onClick={() => connectWithMetamask()} className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
-            Sign In
+          <button onClick={() => address ? disconnect() : connectWithMetamask()} className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
+            {address ? 'Sign Out' : 'Sign In'}
           </button>
         </header>
         <hr className="my-2 border" />
+        {address && (
+            <p className='text-center text-sm text-rose-400'>You're logged in with wallet {address.substring(0, 5)}...{address.substring(address.length - 5)}</p>
+        )}
         {/* Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
           <img
