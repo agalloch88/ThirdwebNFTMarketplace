ef0d59341fac1742c973ee3b50320150232a5003
Ryan Kirsch <agalloch88@gmail.com>
2022-04-09 18:40:04 -0400

    add SSR query, props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { urlFor, sanityClient } from '../sanity';
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
     <div className="">
       <Head>
@@ -10,9 +15,47 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className=''>Welcome to my NFT Drop Page</h1>
+      <h1 className="">Welcome to my NFT Drop Page</h1>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `
+  *[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage{
+    asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug {
+    current
+  },
+  },
+  }
+  `
+
+  const collection = await sanityClient.fetch(query)
+  console.log(collections);
+  
+  return {
+    props: {
+      collections,
+    }
+  }
+}
