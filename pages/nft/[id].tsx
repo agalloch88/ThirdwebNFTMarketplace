b890a05cf0dfade7fd0e6c19dc064cffafdd6550
Ryan Kirsch <agalloch88@gmail.com>
2022-04-09 11:59:36 -0400

    project init and setup

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NFTDropPage() {
+  return (
+    <div className="flex h-screen flex-col">
+      {/* Left */}
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div>
+            <h1>Hello</h1>
+        </div>
+      </div>
+      {/* Right */}
+      <div></div>
+    </div>
+  )
+}
+
+export default NFTDropPage
