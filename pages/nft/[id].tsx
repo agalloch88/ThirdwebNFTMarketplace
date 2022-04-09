ebb4561820666073e42081b30cf4de259a553f32
Ryan Kirsch <agalloch88@gmail.com>
2022-04-09 12:53:11 -0400

    style right section content

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -31,7 +31,13 @@ function NFTDropPage() {
           </header>
           <hr className='my-2 border'/>
           {/* Content */}
+          <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
+              <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="apes collection" />
+              <h1 className='text-3xl font-bold lg:text-5xl font-extrabold'>The Thirdweb Ape Society | NFT Drop</h1>
+              <p className='pt-2 text-xl text-green-500'>13 / 21 NFT's Claimed</p>
+          </div>
           {/* Mint Button */}
+          <button className='h-16'>Mint NFT (0.01 ETH)</button>
       </div>
     </div>
   )
